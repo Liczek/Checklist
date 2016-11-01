14396f84cba6076327996f08e2aa1986422deb84
Paweł Liczmański <paweliczek@gmail.com>
2016-11-01 18:51:11 +0100

    day2 s.49

---
+++ Checklist/Checklist/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Paweł Liczmański on 01.11.2016.
+//  Copyright © 2016 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
--- Checklist/Checklist/ChecklistViewController.swift
+++ Checklist/Checklist/ChecklistViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    
+    var row0text = "Walk the dog"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Soccer practice"
+    var row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = true
+    var row2checked = true
+    var row3checked = false
+    var row4checked = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +35,7 @@ class ChecklistViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -31,33 +44,62 @@ class ChecklistViewController: UITableViewController {
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
-        } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
-        } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOSdevelopmnt"
-        } else if indexPath.row % 5 == 3 {
-            label.text = "Soccer practice"
-        } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
+        if indexPath.row == 0 {
+            label.text = row0text
+        } else if indexPath.row == 1 {
+            label.text = row1text
+        } else if indexPath.row == 2 {
+            label.text = row2text
+        } else if indexPath.row == 3 {
+            label.text = row3text
+        } else if indexPath.row == 4 {
+            label.text = row4text
         }
+        
+        configureCheckmark(for: cell, at: indexPath)
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                    cell.accessoryType = .checkmark
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
             }
-            else {
-                    cell.accessoryType = .none
-            }
+            configureCheckmark(for: cell, at: indexPath)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    func configureCheckmark(for cell: UITableViewCell,
+                            at indexPath: IndexPath) {
+        var isChecked = false
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3 {
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
 
 }
 
